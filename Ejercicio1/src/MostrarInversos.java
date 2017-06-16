a4aaf8813730796b9a6659cece14895bb0ab9465
lopa <lopa@lopa-Lenovo-G470>
2017-06-16 17:17:29 -0300

    este es el primer comit que hago

---
+++ Ejercicio1/src/MostrarInversos.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MostrarInversos {
+
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		int[] numeros=new int[10];
+		
+		for(int i=0;i<numeros.length;++i){
+		 System.out.println("Ingrese numero "+(i+1));
+		 numeros[i]=Integer.parseInt(s.nextLine());}
+		
+		s.close();
+		System.out.println("\nNumeros Mostrados en el orden Inverso\n");
+		
+		for(int i=numeros.length;i>0;--i){
+			System.out.println(numeros[i-1]);
+		}
+	}
+
+}
